11870d35b3956a4096bbef6c2656042fd283696c
unknown <jiperezga@unal.edu.co>
2018-10-03 21:51:52 -0500

    Correction moments function

--- R/kurt.R
+++ R/kurt.R
@@ -3,16 +3,14 @@
 #' @description Calcule theoretical kurtosis or excess kurtosis of any continuous distribution.
 #' @seealso \code{\link{Distributions}} for other standard distributions.
 #' @examples
-#' Enter example D: D: D:
-#' @rdname moment
+#' # Enter example D: D: D:
+#' @rdname kurt
 #' @export
 
-kurt <- function(excess = TRUE, dist, param, lowerDomain = -Inf, upperDomain = Inf, rel.tol = 1e-6){
-  CM4 <- moment(k = 4, dist = dist, param = param, lowerDomain = lowerDomain,
-                        upperDomain = upperDomain, central = TRUE, rel.tol = rel.tol)
+kurt <- function(excess = TRUE, dist, param, domain = "realline", rel.tol = 1e-6){
+  CM4 <- momentss(k = 4, dist = dist, param = param, domain = domain, central = TRUE, rel.tol = rel.tol)
   
-  CM2 <- moment(k = 2, dist = dist, param = param, lowerDomain = lowerDomain,
-                upperDomain = upperDomain, central = TRUE, rel.tol = rel.tol)
+  CM2 <- moments(k = 2, dist = dist, param = param, domain = domain, central = TRUE, rel.tol = rel.tol)
   if(excess) kurt <- CM3/CM2^(2) - 3 else kurt <- CM3/CM2^(2)
   
   return(kurt)
